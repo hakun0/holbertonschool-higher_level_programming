b7fe6f0b88ec7ff43c22db3acf34a9b0fb048eef
hakun0 <brithon.saniko@gmail.com>
2024-02-02 16:16:57 +0100

    pytttte

---
+++ python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def element_at(my_list, idx):
+    n = len(my_list) - 1
+    if idx < 0:
+        return None
+    elif idx > n:
+        return None
+    else:
+        return (my_list[idx])
