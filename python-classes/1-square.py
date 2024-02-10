902f484414deac2acecf6408950022bca436b90f
hakun0 <141654699+hakun0@users.noreply.github.com>
2024-02-10 16:43:23 +0100

    Create 1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a square"""
+
+
+class Square:
+    """
+    Initialize a new Square instance
+
+    Parameters:
+        size (int): The size of the square
+    """
+    def __init__(self, size):
+        self.__size = size
