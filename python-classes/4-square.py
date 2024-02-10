bf8cf30b2e0d35f7982fa950351ee1a1bf1ff77b
hakun0 <141654699+hakun0@users.noreply.github.com>
2024-02-10 16:45:37 +0100

    Create 4-square.py

---
+++ python-classes/4-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Define a square class"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0):
+        """
+        Initialize a new Square instance
+
+        Args:
+            size (int): The size of the square
+         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrives the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size
+
+        Args:
+            value (int): The value of the size of the square
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
